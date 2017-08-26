6915d38346a0c53496929a75d9c3aad15b02bd3e
Daniel Phillips <danieledward.phillips@gmail.com>
2017-08-26 13:25:44 -0600

    adds create with strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,8 +13,18 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
     @article.save
     redirect_to article_path(@article)
   end
+
+
+
+private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+
 end
